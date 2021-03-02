21d48cd9594953587762b1ce9b803e318d52b0ba
Nikolay Ladygin <nilard@gmail.com>
2021-03-02 23:12:06 +0200

    Implement Exercise 1

---
+++ web/modules/custom/exercise1/src/Plugin/Block/Exercise1Block.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\exercise1\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides an 'Exercise 1' block.
+ *
+ * @Block(
+ *   id = "exercise1_block",
+ *   admin_label = @Translation("Exercise 1"),
+ * )
+ */
+class Exercise1Block extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Gets previously fetched list of users.
+    $users = \Drupal::state()->get('exercise1.users', []);
+
+    if (!empty($users)) {
+      $rows = [];
+
+      // Converts list of users to tables rows.
+      foreach ($users as $user) {
+        $rows[] = [
+          $user->name,
+          $user->username,
+          \Drupal\Core\Link::fromTextAndUrl($user->email, \Drupal\Core\Url::fromUri('mailto:' . $user->email))->toString(),
+          $user->phone,
+          \Drupal\Core\Link::fromTextAndUrl($user->website, \Drupal\Core\Url::fromUri('http://' . $user->website, ['attributes' => ['target' => '_blank']]))->toString(),
+        ];
+      }
+
+      // Renders table.
+      return [
+        '#theme' => 'table',
+        '#header' => [
+          $this->t('Name'),
+          $this->t('Username'),
+          $this->t('Email'),
+          $this->t('Phone'),
+          $this->t('Website'),
+        ],
+        '#rows' => $rows,
+        '#attributes' => [
+          'class' => [
+            'views-table',
+          ]
+        ]
+      ];
+    }
+    else {
+      // Displays a message to admin with instructions how to enable this block.
+      if (\Drupal::currentUser()->hasPermission('administer site configuration')) {
+        \Drupal::messenger()->addMessage($this->t('Please run cron to fetch list of users.'));
+      }
+
+      return [];
+    }
+  }
+}
